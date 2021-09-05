2541ba18d48edf10903f4f9948919feecae26675
sifudiep <code.sifudiep@gmail.com>
2021-09-05 13:07:17 +0200

    added add-review and edit-reviews sites, not fixed error handling and edit-reviews site is not finished visually.

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars')
-
 const app = express();
 
-const restaurants = {
-    aqua: {
-        name : "Aqua Dinner & Drinks", 
-        reviewText : "Tis pretty good for it's price, innit. Furthermore I do like me some swedish meatballs. I also happen to like schnitzel. This is not an attempt to make the text longer for development purposement... no that would be innaproppiate.", 
-        imgLink : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fplacesoaf.com%2Fyer%2Faqua-dinner-drinks-772941.jpg&f=1&nofb=1",
-        location : "24 Barnarpsgatan, Jönköping",
-        score : 8
-        
-    }
+const sqllite3 = require("sqlite3")
+const db = new sqllite3.Database('storage/database.db')
+
+// MODEL ATTRIBUTES ORDER - 
+// Name, Score, Review, Location, ImageLink
+
+let allRestaurants;
+
+
+function updateAllRestaurants() {
+    db.all("SELECT * FROM Restaurant", (error, restaurants) => {
+        if (error) console.log(error);
+        else { 
+            allRestaurants = restaurants;
+        }
+    })
 }
+
+updateAllRestaurants();
+
+
+
+
+// HANDLER
+app.use(express.urlencoded({extended: false}));
+
 
 
 app.engine('hbs', expressHandlebars({
@@ -20,9 +35,9 @@ app.engine('hbs', expressHandlebars({
     defaultLayout: "primary-layout.hbs"
 }))
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home.hbs", {})
 })
 
@@ -35,9 +50,40 @@ app.get("/contact", (req, res) => {
 })
 
 app.get("/restaurant-review", (req, res) => {
-    res.render("restaurant-review.hbs", restaurants.aqua)
+    res.render("restaurant-review.hbs")
 })
 
+app.get("/all-reviews", (req, res) => {
+    res.render("all-reviews.hbs", {allRestaurants})
+})
+
+app.get("/add-review", (req,res) => {
+    res.render("add-review.hbs")
+})
+
+app.get("/edit-reviews", (req, res) => {
+    updateAllRestaurants();
+    res.render("edit-reviews.hbs", {allRestaurants})
+})
+
+app.post("/update-review", (req,res) => {
+    console.log(`trying to update review for ${req.body.Name}...`);
+    res.render("edit-reviews.hbs")
+})
+
+app.post("/submit-review", (req,res) => {
+    console.log(req.body)
+    db.run(`INSERT INTO Restaurant (Name, Score, Review, Location, ImageLink) VALUES (?, ?, ?, ?, ?) `, [req.body.Name, req.body.Score, req.body.Review, req.body.Location, req.body.ImageLink], (error) => {
+        if (error) console.log(error);
+        else {
+            console.log(`Successfully inserted a row into Restaurant!`);
+        }
+
+    });
+    
+
+    res.render("add-review.hbs");
+});
 
 app.listen(3000, () => {
     console.log("listening on 3000...")
