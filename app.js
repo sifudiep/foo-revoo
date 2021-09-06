4f45e9b50a24a99b33a77f933035ac3272cea449
sifudiep <code.sifudiep@gmail.com>
2021-09-06 20:41:04 +0200

    changed from req.session.auth to saving auth on database instead

--- app.js
+++ app.js
@@ -12,19 +12,53 @@ const db = new sqllite3.Database("storage/database.db")
 // Name, Score, Review, Location, ImageLink
 
 let allRestaurants;
-
+const port = 3000;
+const threeHoursInMilliseconds = 3 * 60 * 60 * 1000;
 
 function updateAllRestaurants() {
     db.all("SELECT * FROM Restaurant", (error, restaurants) => {
-        if (error) console.log(error);
+        if (error) throw error;
         else {
             allRestaurants = restaurants;
         }
     })
 }
 
-updateAllRestaurants();
+// Checks authentication from database instead of req.session, should be better for memory leaks?
+function checkAuthentication(session) {
+    db.get("SELECT * FROM Session WHERE sid = ?", [session.id], (error, result) => {
+        if (error) throw error;
+        else {
+            if (result) {
+                let sess = JSON.parse(result.sess)
+                if (sess.isAuth) return true;
+            }
+        }
 
+        return false;
+    })
+}
+
+function removeOldSessions() {
+    db.all("SELECT * FROM Session", (error, allSessions) => {
+        if (error) throw error
+        else {
+            for (let i = 0; i < allSessions.length; i++) {
+                let sess = JSON.parse(allSessions[i].sess);
+                let expiry = new Date(sess.cookie.expires);
+                let now = new Date(Date.now());
+                if (now > expiry) {
+                    db.run("DELETE FROM Session WHERE sid = ?", [allSessions[i].sid], (error) => {
+                        if (error) throw error;
+                        else {
+                            console.log(`Deleted session with sid : ${allSessions[i].sid}`);
+                        }
+                    })
+                }
+            }
+        }
+    });
+}
 
 // HANDLER
 app.use(express.urlencoded({
@@ -39,9 +73,10 @@ app.use(session({
     resave: false,
     secret: 'dg$##SK123LE',
     cookie: {
-        maxAge: 180 * 60 * 1000
+        maxAge: threeHoursInMilliseconds
     }
 }))
+
 
 app.engine('hbs', expressHandlebars({
     extname: ".hbs",
@@ -52,13 +87,10 @@ app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.render("home.hbs", {})
-    console.log(`req.session.id : ${req.session.id}`);
-
 })
 
 app.get("/about", (req, res) => {
     res.render("about.hbs", {})
-    console.log(`req.session.id : ${req.session.id}`);
 })
 
 app.get("/contact", (req, res) => {
@@ -76,35 +108,36 @@ app.get("/all-reviews", (req, res) => {
 })
 
 app.get("/add-reviews", (req, res) => {
-    if (req.session.isAuth) {
-        res.render("add-reviews.hbs")
-    } else {
-        res.redirect("./access-denied")
+    if (checkAuthentication(req.session) == false) {
+        res.redirect("/access-denied");
+        return;
     }
+
+    res.render("add-reviews.hbs")
 })
 
 app.get("/edit-reviews", (req, res) => {
-    if (req.session.isAuth) {
-        updateAllRestaurants();
-        res.render("edit-reviews.hbs", {
-            allRestaurants
-        })
-    } else {
-        res.redirect("./access-denied")
+    if (checkAuthentication(req.session) == false) {
+        res.redirect("/access-denied");
+        return;
     }
 
+    updateAllRestaurants();
+    res.render("edit-reviews.hbs", {
+        allRestaurants
+    })
 })
 
 app.get("/delete-reviews", (req, res) => {
-    if (req.session.isAuth) {
-        updateAllRestaurants();
-        res.render("delete-reviews.hbs", {
-            allRestaurants
-        });
-    } else {
-        res.redirect("./access-denied");
-    }
+    if (checkAuthentication(req.session) == false) {
+        res.redirect("/access-denied")
+        return;
+    };
 
+    updateAllRestaurants();
+    res.render("delete-reviews.hbs", {
+        allRestaurants
+    });
 })
 
 app.get("/access-denied", (req, res) => {
@@ -137,14 +170,14 @@ app.get("/:id", (req, res) => {
 
 // Add counter to user to lock account, so you can't have infinite login attempts to account.
 app.post("/login", (req, res) => {
-    db.get("SELECT * FROM USER WHERE Email = ?", [req.body.Email], async (err, user) => {
-        if (err) console.log(err);
+    db.get("SELECT * FROM USER WHERE Email = ?", [req.body.Email], async (error, user) => {
+        if (error) throw error;
         else {
             if (user) {
                 const isMatch = await bcrypt.compare(req.body.Password, user.Password);
                 if (isMatch) {
                     req.session.isAuth = true;
-                    console.log(req.session);
+                    console.log(`succesfully logged in!`);
                 }
             }
             res.redirect("./")
@@ -153,64 +186,69 @@ app.post("/login", (req, res) => {
 })
 
 app.post("/delete-review", (req, res) => {
-    if (req.session.isAuth) {
-        console.log(`posted to delete-review...`);
-        for (let i = 0; i < allRestaurants.length; i++) {
-            if (allRestaurants[i].Id == req.body.Id) {
-                db.run(`DELETE From Restaurant WHERE Id = ?`, [req.body.Id], (error) => {
-                    if (error) console.log(error);
-                    else {
-                        console.log(`Deleted row with Id : ${req.body.Id} and Name : ${req.body.Name}`);
-                    }
-                })
-            }
-        }
-
-        updateAllRestaurants();
-        res.redirect("delete-reviews")
-    } else {
-        res.redirect("access-denied");
+    if (checkAuthentication(req.session) == false) {
+        res.redirect("/access-denied");
+        return;
     }
+
+    for (let i = 0; i < allRestaurants.length; i++) {
+        if (allRestaurants[i].Id == req.body.Id) {
+            db.run(`DELETE FROM Restaurant WHERE Id = ?`, [req.body.Id], (error) => {
+                if (error) throw error;
+                else {
+                    console.log(`Deleted row with Id : ${req.body.Id} and Name : ${req.body.Name}`);
+                }
+            })
+        }
+    }
+
+    updateAllRestaurants();
+    res.redirect("delete-reviews")
 })
 
 app.post("/update-review", (req, res) => {
-    if (req.session.isAuth) {
-        for (let i = 0; i < allRestaurants.length; i++) {
-            if (allRestaurants[i].Id == req.body.Id) {
-                console.log(`updating review for ${req.body.Name}`);
-                db.run(`UPDATE Restaurant SET Name = ?, Score = ?, Review = ?, City = ?, Address = ?, ImageLink = ? WHERE Id = ?`, [req.body.Name, req.body.Score, req.body.Review, req.body.City, req.body.Address, req.body.ImageLink, req.body.Id], (error) => {
-                    if (error) console.log(error);
-                    else {
-                        console.log(`Updated row with Id : ${req.body.Id} and Name : ${req.body.Name}`);
-                    }
-                })
-            }
-        }
-
-        updateAllRestaurants();
-        res.redirect("edit-reviews");
-    } else {
-        res.redirect("access-denied");
+    if (checkAuthentication(req.session) == false) {
+        res.redirect("/access-denied");
+        return;
     }
+
+    for (let i = 0; i < allRestaurants.length; i++) {
+        if (allRestaurants[i].Id == req.body.Id) {
+            console.log(`updating review for ${req.body.Name}`);
+            db.run(`UPDATE Restaurant SET Name = ?, Score = ?, Review = ?, City = ?, Address = ?, ImageLink = ? WHERE Id = ?`, [req.body.Name, req.body.Score, req.body.Review, req.body.City, req.body.Address, req.body.ImageLink, req.body.Id], (error) => {
+                if (error) throw error;
+                else {
+                    console.log(`Updated row with Id : ${req.body.Id} and Name : ${req.body.Name}`);
+                }
+            })
+        }
+    }
+
+    updateAllRestaurants();
+    res.redirect("edit-reviews");
 })
 
 app.post("/add-review", (req, res) => {
-    if (req.session.isAuth) {
-        db.run(`INSERT INTO Restaurant (Name, Score, Review, ImageLink, City, Address) VALUES (?, ?, ?, ?, ?, ?) `, [req.body.Name, req.body.Score, req.body.Review, req.body.ImageLink, req.body.City, req.body.Address], (error) => {
-            if (error) console.log(error);
-            else {
-                console.log(`Successfully inserted a row into Restaurant!`);
-            }
-
-        });
-
-        updateAllRestaurants();
-        res.redirect("add-reviews");
-    } else {
-        res.redirect("access-denied");
+    if (checkAuthentication(req.session) == false) {
+        res.redirect("/access-denied");
+        return; 
     }
+
+    db.run(`INSERT INTO Restaurant (Name, Score, Review, ImageLink, City, Address) VALUES (?, ?, ?, ?, ?, ?) `, [req.body.Name, req.body.Score, req.body.Review, req.body.ImageLink, req.body.City, req.body.Address], (error) => {
+        if (error) throw error;
+        else {
+            console.log(`Successfully inserted a row into Restaurant!`);
+        }
+
+    });
+
+    updateAllRestaurants();
+    res.redirect("add-reviews");
 });
 
-app.listen(3000, () => {
-    console.log("listening on 3000...")
+app.listen(port, () => {
+    console.log("listening on port " + port)
+    setInterval(removeOldSessions, threeHoursInMilliseconds);
+    updateAllRestaurants();
 });
+
